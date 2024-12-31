595fb3ea9cada2e84bee631a073da49491233bd0
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2024-12-31 12:49:41 -0500

    Social changes

--- src/navigation.ts
+++ src/navigation.ts
@@ -40,6 +40,7 @@ export const footerData = {
     { ariaLabel: 'YouTube', icon: 'tabler:brand-youtube', href: 'https://www.youtube.com/@scarcekoi' },
     { ariaLabel: 'Discord', icon: 'tabler:brand-discord', href: 'https://discordapp.com/users/1167818576992936019' },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/scarcekoi' },
+    { ariaLabel: 'Steam', icon: 'tabler:brand-steam', href: 'https://steamcommunity.com/id/scarcekoi/' },
     //{ ariaLabel: 'Mastodon', icon: 'tabler:brand-mastodon', href: 'https://mastodon.social/@scarcekoi' },
   ],
 };
